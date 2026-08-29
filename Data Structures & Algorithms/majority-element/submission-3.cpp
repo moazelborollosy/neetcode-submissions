5dbf924c44af259542f8aa288dfb3866b300dbd6
Moaz Elborollosy <mozaelborollosy@gmail.com>
2026-08-29 14:13:30 +0300

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int maj;
+        int max=0;
+        unordered_map<int,int>m;
+        for(int num:nums){
+            m[num]++;
+        }
+        for(auto const& [key, val] : m){
+            if(val>max){max=val; maj=key;}
+        }
+        return maj;
+    }
+};
